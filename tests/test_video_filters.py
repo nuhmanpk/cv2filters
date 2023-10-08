43aa0fda79d3bf18f725135c0ed6ed463f6573ff
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2023-10-08 13:27:59 +0530

    Update test_video_filters.py

--- tests/test_video_filters.py
+++ tests/test_video_filters.py
@@ -3,13 +3,13 @@ import cv2
 import os
 
 from cv2filters.video_filters import VideoFilters
-from cv2filters.image_filters import ImageFilters
+from cv2filters.image_filters import Filters
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 video_path = os.path.join(script_dir, 'src', 'production_id_4629779(2160p).mp4')
 
 filters = VideoFilters()
-imgFilters = ImageFilters()
+imgFilters = Filters()
 
 class TestVideoFilters(unittest.TestCase):
     def setUp(self):
